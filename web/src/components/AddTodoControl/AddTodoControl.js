30d2553df438907d146fb8769fa31994634d28c5
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2022-04-05 18:47:09 +0200

    removed error handling

--- web/src/components/AddTodoControl/AddTodoControl.js
+++ web/src/components/AddTodoControl/AddTodoControl.js
@@ -1,37 +1,17 @@
-import {
-  FieldError,
-  Form,
-  TextField,
-  Submit,
-  FormError,
-  useForm,
-} from '@redwoodjs/forms'
-import {
-  Button,
-  Input,
-  HStack,
-  FormErrorMessage,
-  FormControl,
-} from '@chakra-ui/react'
+import { Form, TextField, Submit, useForm } from '@redwoodjs/forms'
+import { Button, Input, HStack, FormControl } from '@chakra-ui/react'
 
 import Check from 'src/components/Check'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 
 const AddTodoControl = ({ submitTodo }) => {
   const inputRef = useRef(null)
   const formMethods = useForm()
-  const [error, setError] = useState()
 
   const handleSubmit = (data) => {
-    if (!error) {
-      setError({
-        graphQLErrors: ['Fake error message!'],
-      })
-    } else {
-      submitTodo(data.todo)
-      if (inputRef.current) {
-        inputRef.current.value = ''
-      }
+    submitTodo(data.todo)
+    if (inputRef.current) {
+      inputRef.current.value = ''
     }
   }
 
@@ -40,7 +20,6 @@ const AddTodoControl = ({ submitTodo }) => {
       onSubmit={handleSubmit}
       config={{ mode: 'onBlur' }}
       formMethods={formMethods}
-      error={error}
     >
       <HStack alignItems="center">
         <Check type="plus" />
@@ -50,7 +29,7 @@ const AddTodoControl = ({ submitTodo }) => {
           borderColor="gray.200"
           w="full"
         >
-          <FormControl isInvalid={!!error}>
+          <FormControl>
             <Input
               as={TextField}
               ref={inputRef}
@@ -58,16 +37,8 @@ const AddTodoControl = ({ submitTodo }) => {
               type="text"
               placeholder="Memorize the dictionary"
               variant="unstyled"
-              validation={{
-                required: true,
-                pattern: {
-                  value: /[^@]+@[^\.]+\..+/,
-                },
-              }}
+              required
             />
-            <FormErrorMessage>
-              <FormError error={error} />
-            </FormErrorMessage>
           </FormControl>
           <Button as={Submit} type="submit" colorScheme="purple">
             Add Item
